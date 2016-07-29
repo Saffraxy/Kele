3ba97ad393a94a0e57263db2ecdeb28f42397c12
Sylvia Sievers <ssievers3@gmail.com>
2016-07-29 10:34:29 -0400

    kele-3-retrieveusers completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,15 +3,16 @@ require 'httparty'
 class Kele
   include HTTParty
 
-  #def initialize(attributes = {})
-   #@username = attributes[:username]
-   #@password = attributes[:password]
-   #@BlocAPIURL= "https://www.bloc.io/api/v1"
-  #end
-
   def initialize(email, password)
      response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password})
      raise "invalid email/pass" if response.code != 200
      @auth_token = response["auth_token"]
   end
+
+  def get_me
+    url = "https://www.bloc.io/api/v1/users/me"
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 end
