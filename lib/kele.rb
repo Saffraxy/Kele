1f535e054dbef61c2ecb4a023316ab3718ed7905
Sylvia Sievers <ssievers3@gmail.com>
2016-08-05 13:32:02 -0400

    kele-6-messages completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,4 +25,27 @@ class Kele
     h.to_a
   end
 
+  def get_messages(number = nil)
+    url = "https://www.bloc.io/api/v1/message_threads/"
+    body = {page: number}
+    if number
+      response = self.class.get(url, headers: { "authorization" => @auth_token }, body: body)
+    else
+      response = self.class.get(url, headers: { "authorization" => @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_messages(string, text)
+    url = "https://www.bloc.io/api/v1/messages"
+    body = {
+      user_id: get_me["id"],
+      recipient_id: "539470",
+      subject: string,
+      "stripped-text"=> text
+    }
+    response = self.class.post(url, headers: { "authorization" => @auth_token }, body: body)
+    JSON.parse(response.body)
+  end
+
 end
