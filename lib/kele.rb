896ba739b25c2c7c0d21ae5a8efd7808e72da056
Sylvia Sievers <ssievers3@gmail.com>
2016-08-16 13:28:55 -0400

    kele-7-checkpoint Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -51,11 +51,11 @@ class Kele
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
     url = "https://www.bloc.io/api/v1/checkpoint_submissions"
     body = {
+      checkpoint_id: checkpoint_id,
       assignment_branch: assignment_branch,
       assignment_commit_link: assignment_commit_link,
-      checkpoint_id: checkpoint_id,
       comment: comment,
-      enrollment_id: get_me["current_enrollment_id"]
+      enrollment_id: get_me["current_enrollment"]["id"]
     }
     response = self.class.post(url, headers: { "authorization" => @auth_token }, body: body)
     JSON.parse(response.body)
